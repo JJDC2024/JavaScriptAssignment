27b814f02c951447de7d017808b6e523d8187951
F@llingStars <junelle.james@dcmail.ca>
2024-07-18 15:18:59 -0400

    completed image gallery

--- Image Gallery/main.js
+++ Image Gallery/main.js
@@ -6,17 +6,41 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const orderImages = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
+/* Declaring the alternative text for each image file */
 const altText = ["Big eyeball kid", "Cooled lava", "A group of purple and white flowers", "Ancient Eygptian painting", "Brown butterfly"];
 
-/* Declaring the alternative text for each image file */
-thumbBar
-
 /* Looping through images */
-// for()
+for(let loopImage = 0; loopImage < orderImages.length; loopImage++)
+{
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', orderImages[loopImage]);
+    newImage.setAttribute('alt', altText[loopImage]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', () =>
+    { 
+        displayedImage.setAttribute('src', orderImages[loopImage]);
+        displayedImage.setAttribute('alt', altText[loopImage]);
+    }
+    );
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () =>
+{
+    const newClass = btn.getAttribute("class");
+if (newClass === "dark")
+{
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+}
+
+else {
+    btn.setAttribute("class" , "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+}
+);
