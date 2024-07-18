0c8df3cec4b42bfa37a135f489829116ce0b58fe
F@llingStars <junelle.james@dcmail.ca>
2024-07-18 12:13:23 -0400

    Update main.js

--- Image Gallery/main.js
+++ Image Gallery/main.js
@@ -5,11 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const order = []
+const orderImages = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
+const altText = ["Big eyeball kid", "Cooled lava", "A group of purple and white flowers", "Ancient Eygptian painting", "Brown butterfly"];
 
 /* Declaring the alternative text for each image file */
+thumbBar
 
 /* Looping through images */
+// for()
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
